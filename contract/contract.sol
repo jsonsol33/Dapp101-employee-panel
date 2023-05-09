21e24bd433aad75945cd2d1fd4bccab6d0d74d0a
Colin Stark <collinscodes@gmail.com>
2023-05-09 08:45:18 +0100

    Update contract.sol
    indentation

--- contract/contract.sol
+++ contract/contract.sol
@@ -22,17 +22,17 @@ interface IERC20Token {
 }
 
 contract Payroll {
-/**
-@dev Address of the cUSD token contract
-*/
+    /**
+    @dev Address of the cUSD token contract
+    */
     address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
-/**
-@dev Owner of the contract
-*/
+    /**
+    @dev Owner of the contract
+    */
     address internal owner;
-/**
-@dev Employee struct
-*/
+    /**
+    @dev Employee struct
+    */
     struct Employee {
         string name;
         uint age;
@@ -43,28 +43,28 @@ contract Payroll {
     constructor() {
         owner = msg.sender;
     }
-/**
- * @dev Modifier to check if the caller is the owner of the contract.
- * @notice This modifier is used to restrict certain functions to the owner only.
- */
+    /**
+     * @dev Modifier to check if the caller is the owner of the contract.
+     * @notice This modifier is used to restrict certain functions to the owner only.
+     */
      modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
-/**
-@dev List of employees
-*/
+    /**
+    @dev List of employees
+    */
     Employee[] employees;
 
-/**
- * @dev Add a new employee to the list.
- * @param name Name of the employee.
- * @param age Age of the employee.
- * @param salary Salary of the employee.
- * @param employeeAddress Ethereum address of the employee.
- * @notice This function adds a new employee to the list, but only if the employee does not already exist.
- * @notice Name, age, salary, and employee address must all be provided and meet certain criteria.
- */
+    /**
+    * @dev Add a new employee to the list.
+    * @param name Name of the employee.
+    * @param age Age of the employee.
+    * @param salary Salary of the employee.
+    * @param employeeAddress Ethereum address of the employee.
+    * @notice This function adds a new employee to the list, but only if the employee does not already exist.
+    * @notice Name, age, salary, and employee address must all be provided and meet certain criteria.
+    */
 
     function addEmployee(string memory name, uint age, uint salary, address employeeAddress) public {
         require(bytes(name).length > 0, "Name cannot be empty");
@@ -76,14 +76,15 @@ contract Payroll {
         Employee memory newEmployee = Employee(name, age, salary, employeeAddress);
         employees.push(newEmployee);
     }
-/**
- * @dev Pay an employee's salary in cUSD.
- * @param employeeAddress Ethereum address of the employee.
- * @param salary Amount of cUSD to pay the employee.
- * @notice This function transfers cUSD tokens from the contract owner to the employee.
- * @notice The amount of cUSD to pay must be greater than zero, and the employee address must be valid and associated with an existing employee.
- * @notice The contract owner must have approved the contract to spend the necessary amount of cUSD tokens.
- */
+
+    /**
+     * @dev Pay an employee's salary in cUSD.
+     * @param employeeAddress Ethereum address of the employee.
+     * @param salary Amount of cUSD to pay the employee.
+     * @notice This function transfers cUSD tokens from the contract owner to the employee.
+     * @notice The amount of cUSD to pay must be greater than zero, and the employee address must be valid and associated with an existing employee.
+     * @notice The contract owner must have approved the contract to spend the necessary amount of cUSD tokens.
+     */
     function payEmployees(address employeeAddress, uint salary) public {
       require(employeeAddress != address(0), "Invalid employee address");
       require(salary > 0, "Invalid salary amount");
@@ -97,17 +98,19 @@ contract Payroll {
         bool success = cUsdToken.transferFrom(msg.sender, employeeAddress, salary);
         require(success, "Payment to employee failed");
     }
-/**
-@dev Returns a list of all employees
-@return List of all employees
-*/
+
+    /**
+    @dev Returns a list of all employees
+    @return List of all employees
+    */
     function getEmployees() public view returns (Employee[] memory) {
         return employees;
     }
-/**
-@dev Fires an employee
-@param employeeAddress Address of the employee to be fired
-*/
+
+    /**
+    @dev Fires an employee
+    @param employeeAddress Address of the employee to be fired
+    */
     function fireEmployee(address employeeAddress) public onlyOwner {
     require(employeeAddress != address(0), "Invalid employee address");
     // require(msg.sender == owner, "Only the owner can fire employees");
@@ -127,12 +130,12 @@ contract Payroll {
         return employees.length;
     }
 
-/**
-@dev Allows the owner of the contract to update the salary of a specific employee.
-@param employeeAddress The address of the employee whose salary is being updated.
-@param newSalary The new salary to be updated for the employee.
-@notice No return value.
-*/
+    /**
+    @dev Allows the owner of the contract to update the salary of a specific employee.
+    @param employeeAddress The address of the employee whose salary is being updated.
+    @param newSalary The new salary to be updated for the employee.
+    @notice No return value.
+    */
     function updateSalary(address employeeAddress, uint newSalary) public onlyOwner {
         require(newSalary > 0, "Salary must be greater than 0");
         uint indexToUpdate = findEmployeeIndex(employeeAddress);
